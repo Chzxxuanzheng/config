1d5393f5d3fe23b07020c42ca99ce485676f518d
Mr.Lee <chzxxuanzheng@gmail.com>
2024-11-10 14:57:55 +0800

    增加createLn.sh文件,删除多余文件,把一部分配置文件后缀改为,来获取nvim的高亮

---
+++ createLn.sh
@@ -0,0 +1,6 @@
+ln -s ~/config/fastfetch ~/.config/fastfetch
+ln -s ~/config/nvim ~/.config/nvim/lua
+ln -s ~/config/waybar ~/.config/waybar
+ln -s ~/config/mako.ini ~/.config/mako/config
+ln -s ~/config/kitty.ini ~/.config/kitty/kitty.conf
+ln -s ~/config/tofi.ini ~/.config/tofi/config
